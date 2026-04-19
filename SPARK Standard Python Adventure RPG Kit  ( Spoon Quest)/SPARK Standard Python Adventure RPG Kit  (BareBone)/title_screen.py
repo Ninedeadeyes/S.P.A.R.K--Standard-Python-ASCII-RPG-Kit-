4021b04d81971844816885cde902aa1e075e82ed
Ninedeadeyes <tommykwong_84@hotmail.com>
2026-04-19 19:22:22 +0100

    Add files via upload

---
+++ SPARK Standard Python Adventure RPG Kit  ( Spoon Quest)/SPARK Standard Python Adventure RPG Kit  (BareBone)/title_screen.py
@@ -0,0 +1,16 @@
+
+import animations
+
+
+
+def intro ():
+    print("                          Tile :                       ")
+
+    print("                      Description :  x y z                      ")
+    
+    
+    input("                   press enter to continue "                   )
+    
+    animations.intro_animation()
+    
+
