b69b96ae88a224465bb1782c929518b2e9fb75c3
Ninedeadeyes <tommykwong_84@hotmail.com>
2026-06-16 15:18:12 +0100

    Delete S.P.A.R.K Game Engine directory

--- S.P.A.R.K Game Engine/title_screen.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-import animations
-
-
-def intro() -> None:
-    print("                          The Spoon Quest                        ")
-    print("                Prepare to enter the Dungeon of Doom  !! ")
-    input("                      press enter to continue")
-    animations.intro_animation()
